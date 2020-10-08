6ed97453ab00e195b74b85131f21a53ed749498b
cornwallluke <luke@cornwall-jones.com>
2020-10-08 20:01:48 +0100

    sorry mr gates

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
+if (location.protocol != "https:") {
+    location.href =
+        "https:" +
+        window.location.href.substring(window.location.protocol.length);
+}
 var map;
 function loadMapScenario() {
-    map = new Microsoft.Maps.Map(document.getElementById('myMap'), {});
+    map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
+        mapTypeId: Microsoft.Maps.MapTypeId.ordnanceSurvey
+    });
 }
